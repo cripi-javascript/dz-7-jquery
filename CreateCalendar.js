d739f12108397ec3c78daea7423770095119987a
Гриогорьева Ирина <irina.grigorjeva91@yandex.ru>
2012-11-28 13:14:13 +0600

    Дз7
    заменила на jquery. (кроме предыдущего задания(dz-6-async). Эту часть
    добавлю, как только она заработает)
    работает в IE8
    обернула в domready
    добавление события на страницу сделала с помощью шаблона (функция
    Event.prototype.createSection)

---
+++ CreateCalendar.js
@@ -0,0 +1,79 @@
+﻿/*jslint plusplus: true, browser: true, devel: true */
+var currentEvents = new Events();
+function WriteCalendar() {
+    "use strict";
+    var filterEvents = currentEvents,
+        $filter1,
+        $filter2,
+        $sort,
+        bool,
+        s,
+        res;
+
+    $filter1 = $(".filter1").prop("checked");
+    $filter2 = $(".filter2").prop("checked");
+    bool = false; // флажок. если =true, выдаем отфильтрованную коллекцию. иначе - всю.
+    if ($filter1 === true) {
+        filterEvents = currentEvents.findFutureEvents();
+        bool = !bool;
+    }
+    if ($filter2 === true) {
+        filterEvents = currentEvents.findPastEvents();
+        bool = !bool;
+    }
+
+    s = "";
+    $sort = $('[name="sort"]');
+    $sort.each(function (index, element) {
+        if ($(element).prop("checked") === true) {
+            s = $(element).val();
+        }
+    });
+
+    if (bool) {
+        res = new Events(filterEvents.sortBy(s).items);
+    } else {
+        res = new Events(currentEvents.sortBy(s).items);
+    }
+    res.write();
+}
+
+function CreateCalendar() {
+    "use strict";
+    var $s = $(".start_date").val(),// строка даты 
+        $st = $(".start_time").val(), // строка времени
+        startEv = $s + "T" + $st + ":00",
+        endEv,
+        element;
+
+    $s = $(".end_date").val();
+    $st = $(".end_time").val();
+    endEv = $s + "T" + $st + ":00";
+    element = new Event({
+        start: new Date(startEv),
+        end:  new Date(endEv),
+        name: $(".New_Event").val(),
+        place: $(".plase_event").val(),
+        rating: parseFloat($(".rating_event").val().charAt(0)),
+        comment: $(".comment_event").val(),
+        link: $(".link_event").val()
+    });
+    element.validate();
+    currentEvents.add(element);
+
+    WriteCalendar();
+}
+$(document).ready(function () {
+    "use strict";
+    var $button = $(".add_event"),
+        $filter = $('[name="filter"]'),
+        $sort = $('[name="sort"]'),
+        i;
+    $button.click(CreateCalendar);
+    $filter.each(function (index, element) {
+        $(element).change(WriteCalendar);
+    });
+    $sort.each(function (index, element) {
+        $(element).change(WriteCalendar);
+    });
+});
---
+++ Event.js
@@ -0,0 +1,70 @@
+﻿/*jslint plusplus: true, browser: true, devel: true */
+
+function datatype(data) {// возвращает true, если data имеет тип дата и она корректна
+    "use strict";
+    if (typeof data === 'undefined') {
+        return false;
+    }
+    if (!data.getTime) {
+        return false;
+    }
+    if ('Invalid Date' === data) {
+        return false;
+    }
+    return true;
+}
+
+function ratingtype(rating) {// возвращает true, если rating - число от 0 до 5
+    "use strict";
+    if (typeof rating !== 'number') {
+        return false;
+    }
+    if (rating > 5 || rating < 0) {
+        return false;
+    }
+    return true;
+}
+
+function inherits(Constructor, SuperConstructor) {
+    "use strict";
+    var F = function () {};
+
+    F.prototype = SuperConstructor.prototype;
+
+    Constructor.prototype = new F();
+}
+
+
+// наследуем от Абстракнтого конструктора Model объект Event
+var Event = function (data) {
+    "use strict";
+    Model.apply(this, arguments);
+};
+inherits(Event, Model);
+
+Event.prototype.validate = function () {//проверяет корректность переданных данных.
+    "use strict";
+    if (!datatype(this.start)) {
+        throw new Error(this.start + " не является датой!");
+    }
+    if (!datatype(this.end)) {
+        throw new Error(this.end + " не является датой!");
+    }
+    if (this.start.getTime() - this.end.getTime() > 0) {
+        throw new Error("некорректное событие: не может закончиться раньше, чем начаться!!!");
+    }
+    if (!ratingtype(this.rating)) {
+        throw new Error("введите рейтинг от 0 до 5");
+    }
+};
+
+Event.prototype.createSection = function () {
+    "use strict";
+    var el,
+        markup = "<p>Событие: ${Name}</p><p>Начало: ${Start}</p><p>Конец: ${End}</p><p>Продолжительность: ${Length}</p><p>Рейтинг: ${Rating}</p><p>Место: ${Place}</p><p>Комментарий: ${Comment}</p><p>Сcылка: ${Link}</p><br>",
+        event_temp = [{Name: this.name, Start: this.start, End: this.end, Length: hours(this.end - this.start), Rating: this.rating, Place: this.place, Comment: this.comment, Link: this.link}];
+    el = document.createElement('section');
+    $.template("eventTemplate", markup);
+    $.tmpl("eventTemplate", event_temp).appendTo(el);
+    return el;
+};
